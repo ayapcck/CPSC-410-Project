2f074ce630462634ae9bafdd051ef886b519f179
ayapcck <ayapcck@gmail.com>
2019-10-04 23:13:58 -0700

    add missed 'main.' removal

--- backend/main/sheets_api/SheetsQuickstart.java
+++ backend/main/sheets_api/SheetsQuickstart.java
@@ -1,4 +1,4 @@
-package main.sheets_api;
+package sheets_api;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
