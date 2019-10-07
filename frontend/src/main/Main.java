c14ae042a4cc72f1a7b2e5870af1a4ff15ef9cf9
Andy Tsang <atsang36@hotmail.com>
2019-10-06 20:26:12 -0700

    included main file

---
+++ frontend/src/main/Main.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        List<String> literals = Arrays.asList("create", "date", "expenses", "account_balance", "budget", "track");
+        tokenizer.Tokenizer.makeTokenizer("/Users/AndyTsang/Dropbox/CPSC 410/CPSC-410-Project/frontend/src/main/input.tdot",literals);
+    }
+
+}
