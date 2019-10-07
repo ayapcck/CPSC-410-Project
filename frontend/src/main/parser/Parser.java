9b4dc1a1c63e7f2f990456e90ef241abb19e1cbb
Megha Singhania <meghasinghania999@gmail.com>
2019-10-07 00:31:10 -0700

    Update Parser.java
    Added the parsing for the monthly budget

--- frontend/src/main/parser/Parser.java
+++ frontend/src/main/parser/Parser.java
@@ -3,15 +3,12 @@ package parser;
 import java.util.*;
 
 import ast.*;
+import ast.Date;
 import tokenizer.*;
 
 public class Parser {
     private Parser theParser;
     private Tokenizer theTokenizer = Tokenizer.getTokenizer();
-
-    private Parser(List<String> tokenList) {
-
-    }
 
     public Program parse() {
         return Program();
@@ -23,7 +20,7 @@ public class Parser {
 
     public void createParser(List<String> tokens) {
         if (theParser == null) {
-            theParser = new Parser(tokens);
+            theParser = new Parser();
         }
     }
 
@@ -38,7 +35,7 @@ public class Parser {
                 sheetList.add(s);
             }
         }
-        return new Program(null);
+        return new Program(sheetList);
     }
 
     private Sheet Sheet() {
@@ -72,7 +69,29 @@ public class Parser {
     }
 
     private MonthlyBudget MonthlyBudget() {
-        return null;
+        MonthlyBudgetBlock block;
+        theTokenizer.getAndCheckTokenValue("{");
+        block = MonthlyBudgetBlock();
+        theTokenizer.getAndCheckTokenValue("}");
+        return new MonthlyBudget(block);
+    }
+
+    private MonthlyBudgetBlock MonthlyBudgetBlock() {
+        AST ast;
+        Date date = null;
+        ExpensesBlock expensesBlock = null;
+
+        // We expect two keys in Monthly Budget
+        for (int i = 0; i < 2; i++) {
+            ast = checkBudgetKey(theTokenizer.nextToken());
+
+            if (ast instanceof Date) {
+                date = (Date) ast;
+            } else if (ast instanceof ExpensesBlock) {
+                expensesBlock = (ExpensesBlock) ast;
+            }
+        }
+        return new MonthlyBudgetBlock(date, expensesBlock);
     }
 
     private Trends Trends() {
@@ -85,5 +104,92 @@ public class Parser {
 
     private CourseTracker CourseTracker() {
         return null;
+    }
+
+    private ExpensesBlock ExpensesBlock() {
+        Map<String, ExpenseDetailBlock> expenseDetailBlockMap = new LinkedHashMap<>();
+        String curr;
+        String next = "";
+
+        theTokenizer.getAndCheckTokenValue("{");
+
+        while(!next.equals("}")) {
+            curr = theTokenizer.nextToken();
+            curr = curr.replace("\"", "");
+            next = theTokenizer.nextToken();
+
+            if (next.equals("{")) {
+                expenseDetailBlockMap.put(curr, ExpenseDetailBlock());
+            } else {
+                expenseDetailBlockMap.put(curr, null);
+            }
+        }
+
+        theTokenizer.getAndCheckTokenValue("}");
+        return new ExpensesBlock(expenseDetailBlockMap);
+    }
+
+    private ExpenseDetailBlock ExpenseDetailBlock() {
+        ExpenseDetailBlock block;
+        int budget = -1;
+        boolean track = false;
+        String token;
+
+        for (int i = 0; i< 2; i++) {
+            token = theTokenizer.nextToken();
+
+            if (token.equals("budget")) {
+                theTokenizer.getAndCheckTokenValue(":");
+                budget = parseToInt(theTokenizer.nextToken());
+            } else if (token.equals("track")) {
+                theTokenizer.getAndCheckTokenValue(":");
+                track = parseToBoolean(theTokenizer.nextToken());
+            }
+        }
+        theTokenizer.getAndCheckTokenValue("}");
+        return new ExpenseDetailBlock(budget, track);
+    }
+
+    private ast.Date Date() {
+        String month = theTokenizer.nextToken();
+        int year = parseToInt(theTokenizer.nextToken());
+        return new Date(month, year);
+    }
+
+    private AST checkBudgetKey(String token) {
+        AST value = null;
+        theTokenizer.getAndCheckTokenValue(":");
+        switch(token) {
+            case "date":
+                value = Date();
+                break;
+            case "expenses":
+                value = ExpensesBlock();
+                break;
+        }
+        return value;
+    }
+
+
+    private int parseToInt(String s) {
+        int value = 0;
+        try {
+            value = Integer.parseInt(s);
+        } catch(Exception e) {
+            System.out.println("Invalid value encountered");
+            System.exit(1);
+        }
+        return value;
+    }
+
+    private boolean parseToBoolean(String s) {
+        boolean value = false;
+        try {
+            value = Boolean.parseBoolean(s);
+        } catch(Exception e) {
+            System.out.println("Invalid value encountered");
+           System.exit(1);
+        }
+        return value;
     }
 }
